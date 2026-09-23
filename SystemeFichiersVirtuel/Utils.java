e9aa3ab8e24cad5c82574e08cdf9a7d5fc857062
RUBEN SIMOES <ruben.simoes@iut.local>
2026-09-23 16:23:20 +0200

    retirer les commentaires TODO

--- SystemeFichiersVirtuel/Utils.java
+++ SystemeFichiersVirtuel/Utils.java
@@ -1,12 +1,10 @@
 public class Utils {
 
     public static int writeInt(byte[] memory, int offset, int value) {
-        // TODO: Écrire les 4 octets de 'value' dans 'memory'
         memory[offset]= (byte)((value >> 24) & 0xFF);
         memory[offset+1]= (byte)((value >> 16) & 0xFF);
         memory[offset+2]= (byte)((value >> 8) & 0xFF);
         memory[offset+3]= (byte)(value & 0xFF);
-        // à partir de 'offset', en big-endian.
         return 4;
     }
 
@@ -19,14 +17,12 @@ public class Utils {
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
-        // TODO: Écrire les 2 octets de 'value'.
         memory[offset]= (byte)((value >> 8) & 0xFF);
         memory[offset+1]= (byte)(value & 0xFF);
         return 2;
     }
 
     public static short readShort(byte[] memory, int offset) {
-        // TODO: Lire le short sur 2 octets.
         int value = memory[offset] & 0xFF;
         value = (value << 8) | (memory[offset + 1] & 0xFF);
         return (short) value;
