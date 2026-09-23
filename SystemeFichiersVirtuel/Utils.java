978fc1ebe1e4374f0be5b64dbbcc171b01428733
RUBEN SIMOES <ruben.simoes@iut.local>
2026-09-23 16:15:29 +0200

    réalisation et correction des read and write dans Utils.java

--- SystemeFichiersVirtuel/Utils.java
+++ SystemeFichiersVirtuel/Utils.java
@@ -2,36 +2,33 @@ public class Utils {
 
     public static int writeInt(byte[] memory, int offset, int value) {
         // TODO: Écrire les 4 octets de 'value' dans 'memory'
-        memory[offset]= (byte)((value >> 8) & 0xFF);
+        memory[offset]= (byte)((value >> 24) & 0xFF);
         memory[offset+1]= (byte)((value >> 16) & 0xFF);
-        memory[offset+2]= (byte)((value >> 24) & 0xFF);
-        memory[offset+3]= (byte)((value >> 32) & 0xFF);
+        memory[offset+2]= (byte)((value >> 8) & 0xFF);
+        memory[offset+3]= (byte)(value & 0xFF);
         // à partir de 'offset', en big-endian.
         return 4;
     }
 
     public static int readInt(byte[] memory, int offset) {
-        int value;
-        value = memory[0];
-        value <<= 8;
-        value += memory[1];
-        value <<= 8;
-        value += memory[2];
-        value <<= 8;
-        value += memory[3];
-        value <<= 8;
+        int value = memory[offset] & 0xFF;
+        value = (value << 8) | (memory[offset + 1] & 0xFF);
+        value = (value << 8) | (memory[offset + 2] & 0xFF);
+        value = (value << 8) | (memory[offset + 3] & 0xFF);
         return value;
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
         // TODO: Écrire les 2 octets de 'value'.
         memory[offset]= (byte)((value >> 8) & 0xFF);
-        memory[offset+1]= (byte)((value >> 8) & 0xFF);
+        memory[offset+1]= (byte)(value & 0xFF);
         return 2;
     }
 
     public static short readShort(byte[] memory, int offset) {
         // TODO: Lire le short sur 2 octets.
-        return 0;
+        int value = memory[offset] & 0xFF;
+        value = (value << 8) | (memory[offset + 1] & 0xFF);
+        return (short) value;
     }
 }
