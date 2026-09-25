90cc3b997d482b89b86e29bd5c3e7dcdac33d7c7
Adima mogus <ruben.simoes@iut-rodez.fr>
2026-09-25 17:55:44 +0200

    TP2 vendredi soir

--- SystemeFichiersVirtuel/Utils.java
+++ SystemeFichiersVirtuel/Utils.java
@@ -27,4 +27,50 @@ public class Utils {
         value = (value << 8) | (memory[offset + 1] & 0xFF);
         return (short) value;
     }
+
+    public static int writeLong(byte[] memory, int offset, long value) {
+        for (int i = 7; i >= 0; i--) {
+            memory[offset + (7 - i)] = (byte) ((value >> (i * 8)) & 0xFF);
+        }
+        return 8;
+    }
+
+    public static long readLong(byte[] memory, int offset) {
+        long value = 0;
+        for (int i = 0; i < 8; i++) {
+            value = (value << 8) | (memory[offset + i] & 0xFF);
+        }
+        return value;
+    }
+
+    public static int writeString(
+            byte[] memory,
+            int offset,
+            String str,
+            int maxLength) {
+
+        byte[] bytes = str.getBytes();
+        int length = Math.min(bytes.length, maxLength);
+
+        for (int i = 0; i < length; i++) { 
+            memory[offset + i] = bytes[i]; 
+        }
+        for (int i = length; i < maxLength; i++) {
+            memory[offset + i] = 0; 
+        }
+
+        return maxLength;
+    }
+
+    public static String readString(
+            byte[] memory,
+            int offset,
+            int maxLength) {
+
+        int length = 0;
+        while (length < maxLength && memory[offset + length] != 0) {
+            length++; 
+        }
+        return new String(memory, offset, length);
+    }   
 }
